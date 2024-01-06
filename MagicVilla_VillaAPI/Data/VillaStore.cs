f0f098ff081bb1cd51b16f14a0af1c0f7d10808b
pavangopal <pavangopal913@gmail.com>
2024-01-06 13:45:58 +0530

    Crud operations done

---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO{Id = 1, Name ="Pool View", occupancy = 3, sqft = 90},
+                new VillaDTO{Id = 2, Name = "Beach View", occupancy = 5 , sqft = 150}
+            };
+    }
+}
